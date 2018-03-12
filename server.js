e17522bc791cd399c50ad0d6fb773d4885735963
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-03-12 15:44:06 -0600

    Use express static to serve html

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
 
 app.set('port', process.env.PORT || 3000);
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.listen(app.get('port'), () => {
   console.log('Server running')
 });
-
-app.use(bodyParser.json());
