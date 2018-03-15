b7ff54995faa8dfb799c0595f9e82e03b7cbc3c4
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-03-15 14:49:58 -0600

    Setup teseting environment

--- knexfile.js
+++ knexfile.js
@@ -12,5 +12,16 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     setNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/palettepicker_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
   }
 };
